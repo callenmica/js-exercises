d165daa4f4d44d30371616a45d6f17d10dffbbb3
Callen Mica <callenmicah@gmail.com>
2025-08-02 23:36:14 +0800

    Lesson 11

--- L11-arrays-and-loops/todo-list.js
+++ L11-arrays-and-loops/todo-list.js
@@ -6,7 +6,23 @@ Steps (Algorithm)
 4. Add it to array
 5. console.log() the array
 */
-const todoList = [];
+const todoList = ["make dinner", "climbing"];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+  }
+
+  console.log(todoListHTML);
+
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+}
 
 function addTodo() {
   const inputElement = document.querySelector(".js-todo-input");
@@ -15,5 +31,7 @@ function addTodo() {
   todoList.push(todo);
   console.log(todoList);
 
-  inputElement.value = '';
+  inputElement.value = "";
+
+  renderTodoList();
 }
